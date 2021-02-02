341c4fd5a01dcdfd5b41a3dd3b925f3f6d679819
Barbara Peric <b.peric10@gmail.com>
2021-02-02 12:52:06 +0100

    FEAT: added Bar component

---
+++ src/data/_barData.js
@@ -0,0 +1,30 @@
+export const Weatherdata = [
+  {
+    month: 'Jan', 
+    rainfall: 40,
+  },
+  {
+    month: 'Feb', 
+    rainfall: 20,
+  },
+  {
+    month: 'Mar', 
+    rainfall: 30,
+  },
+  {
+    month: 'Apr', 
+    rainfall: 10,
+  },
+  {
+    month: 'May', 
+    rainfall: 15,
+  },
+  {
+    month: 'Jun', 
+    rainfall: 20,
+  },
+  {
+    month: 'Jul', 
+    rainfall: 40,
+  },
+];
